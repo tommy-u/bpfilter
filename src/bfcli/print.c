00a651cab8bd74ad3f3870db294bce14c7694bc1
Tommy Unger <tunger10@gmail.com>
2025-03-24 23:21:08 -0400

    Remove todos, no-lint instructions, and unnecessary include

--- src/bfcli/print.c
+++ src/bfcli/print.c
@@ -13,12 +13,14 @@
 #include "core/chain.h"
 #include "core/counter.h"
 #include "core/helper.h"
+#include "core/hook.h"
 #include "core/list.h"
 #include "core/logger.h"
 #include "core/marsh.h"
+#include "core/matcher.h"
 #include "core/rule.h"
+#include "core/verdict.h"
 
-// TODO: Obviously resolve this duplication of bf_dump_hex
 #define BF_DUMP_HEXDUMP_LEN 8
 #define BF_DUMP_TOKEN_LEN 5
 
@@ -51,8 +53,6 @@ static void bf_dump_hex_local(const void *data, size_t len)
  * @param with_counters Boolean flag indicating whether to include counters in the dump.
  * @param counter Pointer to the array of counters associated with the chain. Must be non-NULL if with_counters is true.
  */
-// TODO: Sort out our use of fprintf vs DUMP
-// NOLINTBEGIN
 static void bf_cli_chain_dump(struct bf_chain *chain, bool with_counters,
                               struct bf_counter **counter)
 {
@@ -61,62 +61,62 @@ static void bf_cli_chain_dump(struct bf_chain *chain, bool with_counters,
 
     struct bf_hook_opts *opts = &chain->hook_opts;
 
-    fprintf(stderr, "chain %s", bf_hook_to_str(chain->hook));
-    fprintf(stderr, "{");
+    (void)fprintf(stderr, "chain %s", bf_hook_to_str(chain->hook));
+    (void)fprintf(stderr, "{");
 
-    fprintf(stderr, "attach=%s,", opts->attach ? "yes" : "no");
-    fprintf(stderr, "ifindex=%d", opts->ifindex);
+    (void)fprintf(stderr, "attach=%s,", opts->attach ? "yes" : "no");
+    (void)fprintf(stderr, "ifindex=%d", opts->ifindex);
     if (opts->name)
-        fprintf(stderr, ",name=%s", opts->name);
-    fprintf(stderr, "}");
-    fprintf(stderr, " policy: %s\n", bf_verdict_to_str(chain->policy));
+        (void)fprintf(stderr, ",name=%s", opts->name);
+    (void)fprintf(stderr, "}");
+    (void)fprintf(stderr, " policy: %s\n", bf_verdict_to_str(chain->policy));
 
     if (with_counters) {
         // Policy counter is the first one after the rules; error counter follows it.
         struct bf_counter *chain_counter =
             *counter + bf_list_size(&chain->rules);
-        fprintf(stderr, "\tcounters: policy %lu bytes %lu packets; ",
-                chain_counter->bytes, chain_counter->packets);
+        (void)fprintf(stderr, "\tcounters: policy %lu bytes %lu packets; ",
+                      chain_counter->bytes, chain_counter->packets);
 
         chain_counter++;
 
-        fprintf(stderr, "error %lu bytes %lu packets\n", chain_counter->bytes,
-                chain_counter->packets);
+        (void)fprintf(stderr, "error %lu bytes %lu packets\n",
+                      chain_counter->bytes, chain_counter->packets);
     }
 
     // Loop over rules
     bf_list_foreach (&chain->rules, rule_node) {
         struct bf_rule *rule = bf_list_node_get_data(rule_node);
 
-        fprintf(stderr, "\trule: %d\n", rule->index);
+        (void)fprintf(stderr, "\trule: %d\n", rule->index);
         // Matchers
-        fprintf(stderr, "\t\tmatcher(s):\n");
+        (void)fprintf(stderr, "\t\tmatcher(s):\n");
         bf_list_foreach (&rule->matchers, matcher_node) {
             struct bf_matcher *matcher = bf_list_node_get_data(matcher_node);
-            fprintf(stderr, "\t\t\t\%s", bf_matcher_type_to_str(matcher->type));
-            fprintf(stderr, " %s ", bf_matcher_op_to_str(matcher->op));
+            (void)fprintf(stderr, "\t\t\t\%s",
+                          bf_matcher_type_to_str(matcher->type));
+            (void)fprintf(stderr, " %s ", bf_matcher_op_to_str(matcher->op));
 
             bf_dump_hex_local(matcher->payload,
                               matcher->len - sizeof(struct bf_matcher));
-            fprintf(stderr, "\n");
+            (void)fprintf(stderr, "\n");
         }
 
         if (with_counters && rule->counters) {
-            fprintf(stderr, "\t\tcounters: %lu bytes %lu packets\n",
-                    (*counter)->bytes, (*counter)->packets);
+            (void)fprintf(stderr, "\t\tcounters: %lu bytes %lu packets\n",
+                          (*counter)->bytes, (*counter)->packets);
             (*counter)++;
         }
 
-        fprintf(stderr, "\t\tverdict: %s\n", bf_verdict_to_str(rule->verdict));
+        (void)fprintf(stderr, "\t\tverdict: %s\n",
+                      bf_verdict_to_str(rule->verdict));
     }
 
     // Skip over the policy and error counters
     (*counter) += 2;
 
-    fprintf(stderr, "\n");
+    (void)fprintf(stderr, "\n");
 }
-
-// NOLINTEND
 
 int bf_cli_dump_ruleset(struct bf_marsh *chains_and_counters_marsh,
                         bool with_counters)
